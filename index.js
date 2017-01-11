bb2ad126e1c40933b3350e624cedcc807da3e420
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2017-01-10 17:43:41 -0800

    Inline methods.

--- index.js
+++ index.js
@@ -4,19 +4,11 @@ var AWS = require('aws-sdk');
 
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
-var getHtmlFromS3 = function(s3, params, callback) {
-	s3.getObject(params, callback);
-}
-
 var convertToPdf = function(htmlUtf8, event, callback) {
 	var memStream = new MemoryStream();
 	wkhtmltopdf(htmlUtf8, event.options, function(code, signal) {
 		callback(memStream.read());
 	}).pipe(memStream);
-}
-
-var writePdfToS3 = function(s3, params, callback) {
-	s3.putObject(params, callback);
 }
 
 exports.handler = function(event, context) {
@@ -30,12 +22,12 @@ exports.handler = function(event, context) {
 		};
 
 		var s3 = new AWS.S3();
-		getHtmlFromS3(s3, params, function(err, data) {
+		s3.getObject(params, function(err, data) {
 			var htmlUtf8 = data.Body.toString('utf8');
 			convertToPdf(htmlUtf8, event, function(pdf) {
 				params.Body = pdf;
 				params.Key = params.Key + ".pdf";
-				writePdfToS3(s3, params, function(err, data) {
+				s3.putObject(params, function(err, data) {
 					if (err) {
 						console.log(err)
 					} else {
