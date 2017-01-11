73dbf382ba94f7415537d5c01ca964d80c4f04a7
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2017-01-10 17:21:32 -0800

    Extract method.

--- index.js
+++ index.js
@@ -4,25 +4,28 @@ var AWS = require('aws-sdk');
 
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
-var convertToPdf = function(htmlUtf8, fileName, event, context, s3) {
+var getHtmlFromS3 = function(s3, bucketName, fileName, callback) {
+	s3.getObject({
+		Bucket: bucketName,
+		Key: fileName
+	}, callback);
+}
+
+var convertToPdf = function(htmlUtf8, bucketName, fileName, event, context, s3, callback) {
 	var memStream = new MemoryStream();
 	wkhtmltopdf(htmlUtf8, event.options, function(code, signal) {
-		var pdf = memStream.read();
-
-		var params = {
-			Bucket : "tc-html-to-pdf",
-			Key : fileName + ".pdf",
-			Body : pdf
-		}
-
-		s3.putObject(params, function(err, data) {
-			if (err) {
-				console.log(err)
-			} else {
-				context.done(null, { pdf_base64: pdf.toString('base64') });
-			}
-		});
+		callback(memStream.read());
 	}).pipe(memStream);
+}
+
+var writePdfToS3 = function(s3, bucketName, fileName, pdf, callback) {
+	var params = {
+		Bucket : bucketName,
+		Key : fileName + ".pdf",
+		Body : pdf
+	}
+
+	s3.putObject(params, callback);
 }
 
 exports.handler = function(event, context) {
@@ -31,12 +34,17 @@ exports.handler = function(event, context) {
     var fileName = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
 
 		var s3 = new AWS.S3();
-		s3.getObject({
-			Bucket: bucketName,
-			Key: fileName
-		}, function(err, data){
+		getHtmlFromS3(s3, bucketName, fileName, function(err, data) {
 			var htmlUtf8 = data.Body.toString('utf8');
-			convertToPdf(htmlUtf8, fileName, event, context, s3);
+			convertToPdf(htmlUtf8, bucketName, fileName, event, context, s3, function(pdf) {
+				writePdfToS3(s3, bucketName, fileName, pdf, function(err, data) {
+					if (err) {
+						console.log(err)
+					} else {
+						context.done(null, { pdf_base64: pdf.toString('base64') });
+					}
+				})
+			});
 		});
 	}
 };
