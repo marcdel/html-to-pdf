b9a97230f5453a2630fa97d624af52de44cc3e39
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2017-01-10 17:40:59 -0800

    Remove redundant parameters.

--- index.js
+++ index.js
@@ -4,27 +4,18 @@ var AWS = require('aws-sdk');
 
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
-var getHtmlFromS3 = function(s3, bucketName, fileName, callback) {
-	s3.getObject({
-		Bucket: bucketName,
-		Key: fileName
-	}, callback);
+var getHtmlFromS3 = function(s3, params, callback) {
+	s3.getObject(params, callback);
 }
 
-var convertToPdf = function(htmlUtf8, bucketName, fileName, event, context, s3, callback) {
+var convertToPdf = function(htmlUtf8, event, callback) {
 	var memStream = new MemoryStream();
 	wkhtmltopdf(htmlUtf8, event.options, function(code, signal) {
 		callback(memStream.read());
 	}).pipe(memStream);
 }
 
-var writePdfToS3 = function(s3, bucketName, fileName, pdf, callback) {
-	var params = {
-		Bucket : bucketName,
-		Key : fileName + ".pdf",
-		Body : pdf
-	}
-
+var writePdfToS3 = function(s3, params, callback) {
 	s3.putObject(params, callback);
 }
 
@@ -33,11 +24,18 @@ exports.handler = function(event, context) {
 		var bucketName = event.Records[0].s3.bucket.name;
     var fileName = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
 
+		var params = {
+			Bucket: bucketName,
+			Key: fileName
+		};
+
 		var s3 = new AWS.S3();
-		getHtmlFromS3(s3, bucketName, fileName, function(err, data) {
+		getHtmlFromS3(s3, params, function(err, data) {
 			var htmlUtf8 = data.Body.toString('utf8');
-			convertToPdf(htmlUtf8, bucketName, fileName, event, context, s3, function(pdf) {
-				writePdfToS3(s3, bucketName, fileName, pdf, function(err, data) {
+			convertToPdf(htmlUtf8, event, function(pdf) {
+				params.Body = pdf;
+				params.Key = params.Key + ".pdf";
+				writePdfToS3(s3, params, function(err, data) {
 					if (err) {
 						console.log(err)
 					} else {
